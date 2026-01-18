acfc0e168c99a86c522c77883e87c99b29fe0061
Ryan Boudreaux <developer1@epicledger.com>
2026-01-17 20:21:39 -0600

    Added parse and now static functions

--- src/Carbon.ts
+++ src/Carbon.ts
@@ -3,4 +3,10 @@ class Carbon {
     constructor(input: any) {
         this._date = new Date(input);
     }
+    public static parse(input: any) {
+        return new Carbon(input);
+    }
+    public static now() {
+        return Carbon.parse(new Date())
+    }
 }
